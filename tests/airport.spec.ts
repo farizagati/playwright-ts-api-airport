25a139cd0cd45c8f87adea2f50dc767535599a96
Fariz Agati <agatifariz@gmail.com>
2025-06-15 20:32:35 +0700

    add test case to cover POST distance

--- tests/airport.spec.ts
+++ tests/airport.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
  test.describe('GET /airports', () => {
+
   test('Return a list of airports with expected structure', async ({ request }) => {
 
     const response = await request.get('/api/airports', {
@@ -67,6 +68,64 @@ import { test, expect } from '@playwright/test';
     expect(attrs).toHaveProperty('longitude', '106.655998');
     expect(attrs).toHaveProperty('altitude', 34);
     expect(attrs).toHaveProperty('timezone', 'Asia/Jakarta');
+  });
+
+  test('should return correct distance between CGK and DPS with expected values', async ({ request }) => {
+    const response = await request.post('/api/airports/distance', {
+      data: {
+        from: 'CGK',
+        to: 'DPS'
+      }
+    });
+
+    // Status validation
+    expect(response.status(), 'expected 200 status').toBe(200);
+
+    const body = await response.json();
+    const resource = body.data;
+ 
+    // Top-level resource structure validation
+    expect(resource).toHaveProperty('id', 'CGK-DPS');
+    expect(resource).toHaveProperty('type', 'airport_distance');
+    expect(resource).toHaveProperty('attributes');
+
+    const attrs = resource.attributes;
+
+    // From airport details
+    const fromAirport = attrs.from_airport;
+    expect(fromAirport).toMatchObject({
+      id: 2501,
+      name: 'Soekarno-Hatta International Airport',
+      city: 'Jakarta',
+      country: 'Indonesia',
+      iata: 'CGK',
+      icao: 'WIII',
+      latitude: '-6.12557',
+      longitude: '106.655998',
+      altitude: 34,
+      timezone: 'Asia/Jakarta'
+    });
+
+    // To airport details
+    const toAirport = attrs.to_airport;
+    expect(toAirport).toMatchObject({
+      id: 3115,
+      name: 'Ngurah Rai (Bali) International Airport',
+      city: 'Denpasar',
+      country: 'Indonesia',
+      iata: 'DPS',
+      icao: 'WADD',
+      latitude: '-8.74817',
+      longitude: '115.167',
+      altitude: 14,
+      timezone: 'Asia/Makassar'
+    });
+
+    // Distance values
+    expect(attrs.kilometers).toBeCloseTo(982.5905287866152, 6);
+    expect(attrs.miles).toBeCloseTo(610.1284149866348, 6);
+    expect(attrs.nautical_miles).toBeCloseTo(530.1868428254182, 6);
+    
   });
 
   
